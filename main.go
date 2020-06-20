30015f7934ec5c8399ddee7d40287f0876de2a8b
Jeremy O'Brien <neutral@fastmail.com>
2020-06-20 13:58:08 -0400

    consistent capitalization

--- main.go
+++ main.go
@@ -260,7 +260,7 @@ func main() {
 		switch resp.StatusCode {
 		case 200:
 			// grab body
-			fmt.Fprintf(os.Stderr, "Using pkgup index\n")
+			fmt.Fprintf(os.Stderr, "using pkgup index\n")
 			r, err := gzip.NewReader(resp.Body)
 			check(err)
 			bodyBytes, err := ioutil.ReadAll(r)
