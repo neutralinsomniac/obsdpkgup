ce5844f41d6e281b15fbf93a08b519112dd8e22d
Jeremy O'Brien <neutral@fastmail.com>
2020-06-20 14:44:16 -0400

    always compress index.pkgup in generate_pkgup_from_current_dir.sh

--- generate_pkgup_from_current_dir.sh
+++ generate_pkgup_from_current_dir.sh
@@ -4,10 +4,9 @@ rm -f index.pkgup
 
 for pkg in $(ls *.tgz)
 do
-	# could use -O option from gtar here to output to stdout instead of writing to tmp file
 	tar -C /tmp/ -xzf $pkg +CONTENTS
-	echo $pkg $(cat /tmp/+CONTENTS |egrep '^@name|^@version|^@wantlib' |sha256 -b) >> index.pkgup
+	echo "$pkg" $(cat /tmp/+CONTENTS |egrep '^@name|^@version|^@wantlib' |sha256 -b) >> index.pkgup
 done
 
 rm -f index.pkgup.gz
-gzip index.pkgup
+gzip -f index.pkgup
