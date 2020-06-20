67d321ca2eee5b164034ec6164dc31897ec769fd
Jeremy O'Brien <neutral@fastmail.com>
2020-06-20 14:13:46 -0400

    pkgup from mirror generation script updates

--- generate_pkgup_from_mirror.sh
+++ generate_pkgup_from_mirror.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+IFS="
+"
+
 mirror=$(cat /etc/installurl)
-arch=$(uname -m)
+arch=$(arch -s)
 
 if (sysctl kern.version |grep -- -current)
 then
@@ -9,23 +13,33 @@ else
 	version=$(sysctl -n kern.version |head -n 1 |cut -b 9-11)
 fi
 
-url=$mirror/$version/packages/$arch
+url="$mirror/$version/packages/$arch"
 
-index=$(curl $url/index.txt)
+index=$(curl $url/index.txt 2>/dev/null)
+
+if [ -e index.pkgup.gz ]
+then
+	local_quirks=$(gunzip -c index.pkgup.gz |grep quirks |cut -d " " -f 1)
+	remote_quirks=$(echo "$index" |grep quirks |cut -b 53-)
+	if [ a$local_quirks = a$remote_quirks ]
+	then
+		echo "no update required"
+		exit
+	fi
+fi
+
+exit
 
 rm -f index.pkgup
 
-IFS="
-"
-
 for line in $index
 do
-	pkg=$(echo $line |cut -b 53-)
+	pkg=$(echo "$line" |cut -b 53-)
 	if echo $pkg |grep '\.tgz$'
 	then
 		curl $url/$pkg |tar xzqf - +CONTENTS
 		hash=$(cat +CONTENTS |egrep '^@name|^@version|^@wantlib' |sha256 -b)
-		echo $pkg $hash >> index.pkgup
+		echo "$pkg" "$hash" >> index.pkgup
 		rm -f +CONTENTS
 	fi
 done
